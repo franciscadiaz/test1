34835f4bbbba649b9fcbf499730d65385e136c99
franciscadiaz <patidizmarmol@hotmail.com>
2021-01-28 20:18:26 +0100

    primer cambio

---
+++ hola name.py
@@ -0,0 +1,24 @@
+
+# -*- coding: utf-8 -*-
+
+"""
+
+Escribir script que pregunte el nombre del ususario en la consola y un número entero  e imprima por pantalla en lineas distintas el nombre de usuario tantas veces como el número introcucido
+
+"""    
+
+
+name = input("¿Cómo te llamas?")
+n = input("Introduce un número entero:")
+print ((name + "\n") * int(n))
+
+
+""" este es otro ejemplo de Mario 
+
+name = input("¿Cómo te llamas?")
+n = int(input("Introduce un número entero:"))
+print ((name + "\n") * int(n))
+
+"""
+
+
