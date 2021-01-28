2cc7f203e768ba5a6ca7d619216d35f061422b79
franciscadiaz <patidizmarmol@hotmail.com>
2021-01-28 20:39:42 +0100

    mi segundo cambio

---
+++ hola name1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Escribir script que pregunte el nombre del ususario en la consola y un número entero  e imprima por pantalla en lineas distintas el nombre de usuario tantas veces como el número introcucido
+
+"""    
+
+
+name = input("Introduce tu nombre:")
+print ("¡Hola " + name + "!")
+
